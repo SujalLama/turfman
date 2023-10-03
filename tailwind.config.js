190f3898a2973696df2c59a161805bd7a589417e
sujal lama <lsujal41@gmail.com>
2023-10-03 13:32:44 +0545

    container and font setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultConfig";
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['Poppins', 'sans-serif'],
+      'display': ['Roboto', 'sans-serif'],
+    },
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+    },
+    container: false,
   },
   plugins: [],
 }
